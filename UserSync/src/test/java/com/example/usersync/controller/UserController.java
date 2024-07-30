a61517b9ba681b4fc3aa66855309d3d663e443b4
turtleYJ <yooil405@gmail.com>
2024-07-30 22:57:55 +0900

    feat: User Create 컨트롤러 TDD

---
+++ UserSync/src/test/java/com/example/usersync/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.example.usersync.controller;
+
+import com.example.usersync.User;
+import com.example.usersync.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.apiguardian.api.API;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+@Api(value = "User Magagement System", description = "Operations pertaining to user in User Management System")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @ApiOperation(value = "Create a new user", response = User.class)
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createdUser(user);
+    }
+}
---
+++ UserSync/src/test/java/com/example/usersync/controller/UserControllerTest.java
@@ -0,0 +1,52 @@
+package com.example.usersync.controller;
+
+import com.example.usersync.User;
+import com.example.usersync.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(UserController.class)
+class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserService userService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private User user;
+
+    @BeforeEach
+    public void setUp() {
+        user = new User();
+        user.setId(1L);
+        user.setName("John Doe");
+    }
+
+    @Test
+    void testCreateUser() throws Exception {
+        given(userService.createdUser(any(User.class))).willReturn(user);
+
+        mockMvc.perform(post("/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(user)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(user.getName())));
+    }
+}
